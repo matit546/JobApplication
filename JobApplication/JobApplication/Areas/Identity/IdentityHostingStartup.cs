54cf7e54d55be085099a3ecb426ccea04cf04fba
Mateusz Twardowski <m.twardowski@student.po.edu.pl>
2020-07-06 17:15:25 +0200

    nuget

---
+++ JobApplication/JobApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JobApplication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JobApplication.Areas.Identity.IdentityHostingStartup))]
+namespace JobApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- JobApplication/JobApplication/Startup.cs
+++ JobApplication/JobApplication/Startup.cs
@@ -34,6 +34,7 @@ namespace JobApplication
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
