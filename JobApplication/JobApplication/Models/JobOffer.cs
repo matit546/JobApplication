5ebdc3abb6129372709323ecef771ceddba5f8d7
Paweł Rogowski <p.rogowski3074@student.po.edu.pl>
2020-07-07 13:41:49 +0200

    Startup

--- JobApplication/JobApplication/Models/JobOffer.cs
+++ JobApplication/JobApplication/Models/JobOffer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,24 @@ namespace JobApplication.Models
 {
     public class JobOffer
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string TypeOfJob { get; set; }
+        public decimal PaymentMin { get; set; }
+        public decimal PaymentMax { get; set; }
+        [Required]
+        public DateTime PublicationTime { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public DateTime Deadline { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string ChooseTheCurrency { get; set; }
     }
 }
--- JobApplication/JobApplication/Startup.cs
+++ JobApplication/JobApplication/Startup.cs
@@ -35,6 +35,34 @@ namespace JobApplication
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
