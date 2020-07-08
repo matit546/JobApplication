4e08bc7abf2843f59a666b8c76c8c0c51fc95037
Paweł Rogowski <p.rogowski3074@student.po.edu.pl>
2020-07-08 12:48:18 +0200

    Bug fix

--- JobApplication/JobApplication/Data/ApplicationDbContext.cs
+++ JobApplication/JobApplication/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using JobApplication.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,6 @@ namespace JobApplication.Data
             : base(options)
         {
         }
+        public DbSet<JobOffer> JobOffers { get; set; }
     }
 }
--- JobApplication/JobApplication/Models/JobOffer.cs
+++ JobApplication/JobApplication/Models/JobOffer.cs
@@ -12,17 +12,24 @@ namespace JobApplication.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [StringLength(40, ErrorMessage = "Tytuł nie może być dłuższy niż 40 znaków")]
         public string Title { get; set; }
         [Required]
         public string Location { get; set; }
         [Required]
         public string TypeOfJob { get; set; }
+        [Range(1, 20000, ErrorMessage = "Wartość maksymalna to 200000")]
+        [DataType(DataType.Currency)]
         public decimal PaymentMin { get; set; }
+        [Range(1, 200000, ErrorMessage = "Wypłata musi mieć wartość przynajmniej 1")]
+        [DataType(DataType.Currency)]
         public decimal PaymentMax { get; set; }
         [Required]
         public DateTime PublicationTime { get; set; }
         [Required]
         public string Category { get; set; }
+        [Required]
+        public string Skills { get; set; }
         [Required]
         public DateTime Deadline { get; set; }
         [Required]
