a11e1b670cc65c1f64f3bb9c72ec192e9249c79c
Paweł Rogowski <p.rogowski3074@student.po.edu.pl>
2020-07-08 16:28:52 +0200

    identity user

--- JobApplication/JobApplication/Areas/Identity/Data/AppUser.cs
+++ JobApplication/JobApplication/Areas/Identity/Data/AppUser.cs
@@ -6,24 +6,41 @@ using System.Threading.Tasks;
 
 namespace JobApplication.Areas.Identity.Data
 {
-    public class AppUser: IdentityUser
+    public class AppUser : IdentityUser
     {
+        [PersonalData]
         public string CompanyName { get; set; }
+        [PersonalData]
         public string Headline { get; set; }
+        [PersonalData]
         public string Website { get; set; }
+        [PersonalData]
         public DateTime FoundingDate { get; set; }
+        [PersonalData]
         public CompanySize CompanySize { get; set; }
+        [PersonalData]
         public string ShortDescription { get; set; }
+        [PersonalData]
         public string Descrption { get; set; }
+        [PersonalData]
         public string Categories { get; set; }
+        [PersonalData]
         public string Address { get; set; }
+        [PersonalData]
         public string VideoUrl { get; set; }
+        [PersonalData]
         public string BackgroundImage { get; set; }
+        [PersonalData]
         public string Gallery { get; set; }
+        [PersonalData]
         public string FacebookProfile { get; set; }
+        [PersonalData]
         public string TwitterProfile { get; set; }
+        [PersonalData]
         public string YoutubeProfile { get; set; }
+        [PersonalData]
         public string VimeoProfile { get; set; }
+        [PersonalData]
         public string LinkedinProfile { get; set; }
 
     }
---
+++ JobApplication/JobApplication/Areas/Identity/Data/AppUserEmployeeExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApplication.Areas.Identity.Data
+{
+    public class AppUserEmployeeExtension
+    {
+        public int ID { get; set; }
+        public string Gender { get; set; }
+        public bool IsShowing { get; set; }
+
+    }
+}
