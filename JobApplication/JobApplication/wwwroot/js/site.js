a2b7e4c5499be0d94b9bea1fb54b1ab0aa343ce4
Jerzy Długosz <dlugoszjerzy98@gmail.com>
2020-07-09 14:05:28 +0200

    Button to swap Kandydat and Pracownik

--- JobApplication/JobApplication/wwwroot/js/site.js
+++ JobApplication/JobApplication/wwwroot/js/site.js
@@ -3,3 +3,39 @@
 
 // Write your JavaScript code.
 
+$(document).ready(function () {
+    $(".button1").click(function () {
+        if ($(this).hasClass('.disabled')) {
+
+        }
+        else {
+            $(".firma").hide("fast");
+            $(this).addClass('disabled');
+            $(".button2").removeClass('disabled');
+            $(".button1").toggleClass("current-menu-item");
+            $(".button2").toggleClass("current-menu-item");
+            $("#NazwaFirmy").val("");
+        }
+    });
+    $(".button2").click(function () {
+        if ($(this).hasClass('disabled')) {
+
+        }
+        else {
+            $(".firma").show("fast");
+            $(this).addClass('disabled');
+            $(".button1").removeClass('disabled');
+            $(".button1").toggleClass("current-menu-item");
+            $(".button2").toggleClass("current-menu-item");
+        }
+    });
+});
+
+$(document).ready(function () {
+    var $thisnav = $('.current-menu-item').offset().left;
+    $('.menu-item').click(function () {
+        var $left = $(this).offset().left - $thisnav;
+        var $width = $(this).outerWidth();
+        $('.wee').css({ 'left': $left, 'width': $width });
+    });
+});
