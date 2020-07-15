cf6b9c0bec086e123d016feebb636c405f6a7fbd
Paweł Rogowski <p.rogowski3074@student.po.edu.pl>
2020-07-15 15:33:55 +0200

    wczytywanie ofert strona główna ale bez css

--- JobApplication/JobApplication/Controllers/HomeController.cs
+++ JobApplication/JobApplication/Controllers/HomeController.cs
@@ -7,21 +7,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using JobApplication.Models;
 using Microsoft.AspNetCore.Authorization;
+using JobApplication.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobApplication.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.JobOffers.ToListAsync());
         }
         [Authorize(Policy = "RequireAdministratorRole")]
         public IActionResult Privacy()
