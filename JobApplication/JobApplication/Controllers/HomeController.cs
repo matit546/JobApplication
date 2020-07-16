b0cb023a521e3c8d92096b88323a367185264020
Paweł Rogowski <p.rogowski3074@student.po.edu.pl>
2020-07-16 15:01:56 +0200

    test zdjecia

--- JobApplication/JobApplication/Controllers/HomeController.cs
+++ JobApplication/JobApplication/Controllers/HomeController.cs
@@ -9,6 +9,10 @@ using JobApplication.Models;
 using Microsoft.AspNetCore.Authorization;
 using JobApplication.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using JobApplication.Areas.Identity.Data;
 
 namespace JobApplication.Controllers
 {
@@ -16,11 +20,13 @@ namespace JobApplication.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _iWebHost;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IWebHostEnvironment webHost)
         {
             _logger = logger;
             _context = context;
+            _iWebHost = webHost;
         }
 
         public async Task<IActionResult> Index()
@@ -28,8 +34,29 @@ namespace JobApplication.Controllers
             return View(await _context.JobOffers.ToListAsync());
         }
         [Authorize(Policy = "RequireAdministratorRole")]
-        public IActionResult Privacy()
+        public async Task <IActionResult> Privacy(IFormFile file, AppUser appUser)
         {
+            if (file != null && file.Length > 0)
+            {
+                var imagePath = @"\img\images\";
+                var uploadPath = _iWebHost.WebRootPath + imagePath;
+
+                if (!Directory.Exists(uploadPath))
+                {
+                    Directory.CreateDirectory(uploadPath);
+                }
+
+                var newFileName = Guid.NewGuid().ToString();
+                var fileName = Path.GetFileName(newFileName + "." + file.FileName.Split(".")[1].ToLower());
+                string fullPath = uploadPath + fileName;
+                imagePath = imagePath + @"\";
+                var filePath = @".." + Path.Combine(imagePath, fileName);
+                using (var fileStream = new FileStream(fullPath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+                appUser.BackgroundImage = fileName;
+            }
             return View();
         }
 
--- JobApplication/JobApplication/wwwroot/js/site.js
+++ JobApplication/JobApplication/wwwroot/js/site.js
@@ -81,3 +81,12 @@ $(window).on("load", function () {
 
 
 //});
+function ShowPreview(input) {
+    if (input.files && input.files[0]) {
+        var ImageDir = new FileReader();
+        ImageDir.onload = function (e) {
+            $('#impPrev').attr('src', e.target.result);
+        }
+        ImageDir.readAsDataURL(input.files[0]);
+    }
+}
