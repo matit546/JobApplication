8e8ffd402d4c0eda453861721b611d11389fa1e6
Jerzy Długosz <dlugoszjerzy98@gmail.com>
2020-07-21 13:05:03 +0200

    merge

--- JobApplication/JobApplication/Areas/Identity/Data/ViewModels/CandidateViewModel.cs
+++ JobApplication/JobApplication/Areas/Identity/Data/ViewModels/CandidateViewModel.cs
@@ -10,9 +10,6 @@ namespace JobApplication.Areas.Identity.Data.ViewModels
     {
         public  AppUserDto AppUserDto { get; set; }
         public AppUserEmployeeExtension AppUserEmployeeExtension{get;set;}
-        public AwardsEmployee awardsEmployee { get; set; }
-        public EducationEmployee educationEmployee { get; set; }
-        public ExperiencesEmployee experiencesEmployee { get; set; }
-        public SkillsEmployee skillsEmployee { get; set; }
+
     }
 }
