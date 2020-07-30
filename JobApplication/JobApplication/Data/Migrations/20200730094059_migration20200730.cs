1d7f9f01aadb9f211c47cddacc159c9c844c6305
Jerzy Długosz <dlugoszjerzy98@gmail.com>
2020-07-30 12:58:58 +0200

    string to int

---
+++ JobApplication/JobApplication/Data/Migrations/20200730094059_migration20200730.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JobApplication.Data.Migrations
+{
+    public partial class migration20200730 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeOfJob",
+                table: "JobOffers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TypeOfJob",
+                table: "JobOffers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
