347183fdfbf8ef7104e1439ca63d288a3ea1b524
Mateusz Twardowski <m.twardowski@student.po.edu.pl>
2020-07-31 13:36:48 +0200

    Edit/Delete/Info JobOffer

--- JobApplication/JobApplication/Models/JobOffer.cs
+++ JobApplication/JobApplication/Models/JobOffer.cs
@@ -21,9 +21,11 @@ namespace JobApplication.Models
         [Required(ErrorMessage = "To pole jest wymagane")]
         [Range(1, 20000, ErrorMessage = "Wartość maksymalna to 200000")]
         [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(15, 2)")]
         public decimal PaymentMin { get; set; }
         [Range(1, 200000, ErrorMessage = "Wypłata musi mieć wartość przynajmniej 1")]
         [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(15, 2)")]
         [Required(ErrorMessage = "To pole jest wymagane")]
         public decimal PaymentMax { get; set; }
         [DataType(DataType.Date)]
