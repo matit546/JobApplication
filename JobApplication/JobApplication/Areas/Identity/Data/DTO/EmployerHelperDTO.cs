2fe3df51bf859e2823e9b4aa09b59f93001a6e79
Paweł Rogowski <p.rogowski3074@student.po.edu.pl>
2020-08-03 12:37:26 +0200

    zmiany

---
+++ JobApplication/JobApplication/Areas/Identity/Data/DTO/EmployerHelperDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApplication.Areas.Identity.Data.DTO
+{
+    public class EmployerHelperDTO
+    {
+        public string Candidate { get; set; }
+        public string DownloadCV { get; set; }
+        public string Source { get; set; }
+        public string Status { get; set; }
+    }
+}
